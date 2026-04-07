19a13ec9549d798818939408afec3a960ab7ba38
yeMorGx <gabrielmcgoes@gmail.com>
2026-04-07 20:09:54 -0300

    chore: update Login component layout with styling and structure

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
 export default function Login() {
-  return (
-    <main>
-        
+    return (
+    <main className="bg-gray-950 h-screen flex items-center justify-center">
+        <div>
+            <h1 className="text-4xl font-bold text-white">Login</h1>
+            <form className="mt-4">
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
     </main>
-  );
+    );
 }
