5721e8b394e3bbbdfc582efb9257c07db3db09f3
Gabriel Morgado <163890467+yeMorGx@users.noreply.github.com>
2026-04-10 21:44:18 -0300

    chore: update Dashboard layout, add FontAwesome icons for navigation, and upgrade Next.js to version 16.2.3

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function Dashboard() {
     return (
-        <div className="bg-gray-950 min-h-screen">
+        <div className="">
             
         </div>
     );
--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Montserrat, Playfair_Display, Doto, Syne_Mono, Bytesized } from "next/font/google";
 import { useEffect, useState } from "react";
@@ -7,6 +9,7 @@ import { useEffect, useState } from "react";
 import { ReactNode } from "react";
 import { faHouse } from '@fortawesome/free-solid-svg-icons/faHouse';
 import { faTicket } from '@fortawesome/free-solid-svg-icons/faTicket';
+import { faGear } from '@fortawesome/free-solid-svg-icons/faGear';
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -21,8 +24,14 @@ export default function AppLayout({ children }) {
                         <div className="border-b border-gray-800">
                             {/* Profile */}
                             <div className='flex items-center justify-center'>
-                                <a href="/profile"><div className='hover:shadow-lg border-1 border-gray-900 absolute right-2 top-8 z-40 bg-green-500 w-2 h-2 m-4 rounded-full'></div></a>
-                                <a href="/profile"><div className='hover:shadow-lg  bg-gray-200 w-10 h-10 m-4 rounded-full'></div></a>
+                                <a href="/profile"><div className='hover:shadow-lg border-1 border-gray-900 absolute right-2 top-8 z-40 bg-green-500 w-2 h-2 m-4 rounded-full'>
+                                    </div></a>
+                                <a href="/profile"><div className='hover:shadow-lg  bg-gray-200 w-10 h-10 m-4 rounded-full'>
+                                    
+                                    
+                                    
+                                    
+                                    </div></a>
                             </div>
                         </div>
                         {/* Navigation */}
@@ -31,13 +40,19 @@ export default function AppLayout({ children }) {
                             <ul className="p-4 flex flex-col gap-4">
                                 {/* Dashboard */}
                                 <a href="/dashboard">
-                                    <li className="bg-blue-500 hover:bg-blue-600 hover:scale-120 transition transform p-3 w-fit h-fit rounded-xl flex items-center justify-center p-2">
+                                    
+                                    <li className="bg-(--cor-principal) hover:bg-(--cor-priHover) hover:rounded-4xl transition-all duration-500 ease-in-out  hover:scale-120 transition transform p-3 w-10 h-10 rounded-xl flex items-center justify-center p-2">
                                         <FontAwesomeIcon icon={faHouse} className="text-white"/>
                                     </li>
                                 </a>
                                 <a href="/ticket">
-                                    <li className="bg-blue-500 hover:bg-blue-600 hover:scale-120 transition transform  p-3 w-fit h-fit rounded-xl flex items-center justify-center p-2">
+                                    <li className="bg-(--cor-principal) hover:bg-(--cor-priHover) hover:rounded-4xl transition-all duration-500 ease-in-out  hover:scale-120 transition transform p-3 w-10 h-10 rounded-xl flex items-center justify-center p-2">
                                         <FontAwesomeIcon icon={faTicket} className="text-white"/>
+                                    </li>
+                                </a>
+                                <a href="/settings">
+                                    <li className="bg-(--cor-principal) hover:bg-(--cor-priHover) hover:rounded-4xl transition-all duration-500 ease-in-out  hover:scale-120 transition transform p-3 w-10 h-10 rounded-xl flex items-center justify-center p-2">
+                                        <FontAwesomeIcon icon={faGear} className="text-white"/>
                                     </li>
                                 </a>
                             </ul>
