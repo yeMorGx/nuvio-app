f31fe2e7a0ad852b80a1a108af2e5ff50814e9d8
yeMorGx <gabrielmcgoes@gmail.com>
2026-05-26 20:05:55 -0300

    feat: implement dashboard layout with responsive sidebar and header components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,29 @@
-'use client'
-
-import "./globals.css"
+import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "next-themes";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning lang="pt-BR" className={cn("font-sans", geist.variable)}>
+    <html
+      suppressHydrationWarning
+      lang="pt-BR"
+      className={cn("font-sans", geist.variable)}
+    >
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
