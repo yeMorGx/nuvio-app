5ea3d2eb3552beb04ff1d24fef2adcc753ba4f67
yeMorGx <gabrielmcgoes@gmail.com>
2026-05-27 19:48:49 -0300

    feat: add BarCharts component and integrate it into Reports page
    - Added BarCharts component using MUI's BarChart for visual representation.
    - Updated Reports page to include BarCharts component.
    - Added dependencies for @emotion/react, @emotion/styled, @mui/material, and @mui/x-charts in package.json.

--- src/app/(app)/reports/page.tsx
+++ src/app/(app)/reports/page.tsx
@@ -1,11 +1,15 @@
+import BarCharts from "@/components/charts/barCharts";
+
+
+
 export default function Reports() {
   return (
     
     <div>
         {/* Div Pai */}
-        
         <div>
-            
+            <BarCharts/>
+
         </div>
     </div>
   );
---
+++ src/components/charts/barCharts.tsx
@@ -0,0 +1,23 @@
+import { BarChart } from "@mui/x-charts/BarChart";
+
+export default function barCharts() {
+  return (
+    <div>
+      <BarChart
+        xAxis={[
+          {
+            id: "barCategories",
+            data: ["bar A", "bar B", "bar C"],
+            height: 28,
+          },
+        ]}
+        series={[
+          {
+            data: [2, 5, 3],
+          },
+        ]}
+        height={300}
+      />
+    </div>
+  );
+}
